cfed3b4aae5c14936c31313dbbcba1abc68db2cd
Ganjala BhavyaSree <ganjalabhavyasri@gmail.com>
2026-06-20 18:10:02 +0530

    Fix routing conflict

--- backend/src/main/java/com/agrigenius/controller/RootController.java
+++ backend/src/main/java/com/agrigenius/controller/RootController.java
@@ -1,8 +1,3 @@
-package com.agrigenius.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class RootController {
 
